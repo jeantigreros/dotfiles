42927cdd22ccefa5cf4d14a79ea82122f0e2d5a9
jeantigreros <jtigreros92@gmail.com>
2025-03-09 11:20:59 -0500

    fish vi keybindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,35 +14,35 @@ function config
     /usr/bin/git --git-dir=/home/j/.cfg --work-tree=/home/j $argv
 end
 
-function ccs 
+function ccs
     config status
 end
 
-function ccm 
+function ccm
     config commit -m "$argv"
 end
 
-function ccp 
+function ccp
     config push
 end
 
-function cca 
+function cca
     config add $argv
 end
 
-function ggs 
+function ggs
     git status
 end
 
-function ggm 
+function ggm
     git commit -m "$argv"
 end
 
-function ggp 
+function ggp
     git push
 end
 
-function gga 
+function gga
     git add $argv
 end
 
@@ -52,13 +52,13 @@ export EDITOR="nvim"
 
 # binds
 
-bind \cl forward-char 
+bind \cl forward-char
 bind \el clear-screen
 
 # pnpm
 set -gx PNPM_HOME "/home/j/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
 
@@ -71,3 +71,6 @@ set -gx PATH $PATH /home/j/.lmstudio/bin
 
 # Created by `pipx` on 2025-02-18 00:42:11
 set PATH $PATH /home/j/.local/bin
+
+# enable vim keybindings
+fish_vi_key_bindings
