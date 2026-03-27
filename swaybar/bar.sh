b63e055c8dec8b5a3334d317ea1f0a533fd1bbff
Jean Carlos Tigreros <jtigreros92@gmail.com>
2026-03-26 20:56:58 -0500

    spell and lint

--- swaybar/bar.sh
+++ swaybar/bar.sh
@@ -1,39 +1,39 @@
 #!/bin/sh
- 
+
 common() {
-echo -n "{"
-echo -n "\"color\":\"#ffffff\","
-echo -n "\"background\":\"$bg\","
-echo -n "\"name\":\"$name\","
-echo -n "\"full_text\":\" $stat\","
-echo -n "},"
+  echo -n "{"
+  echo -n "\"color\":\"#ffffff\","
+  echo -n "\"background\":\"$bg\","
+  echo -n "\"name\":\"$name\","
+  echo -n "\"full_text\":\" $stat\","
+  echo -n "},"
 }
- 
+
 mydate() {
-local name="id_time"
-local stat=" $(date "+%a-%b-%d %I:%M %p")"
-common
+  local name="id_time"
+  local stat=" $(date "+%a-%b-%d %I:%M %p")"
+  common
 }
- 
+
 volume() {
-    local name="id_volume"
-    bg=""
+  local name="id_volume"
+  bg=""
 
-    # Get volume as float (0–1)
-    vol_float=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2}')
-    vol_percent=$(awk -v v="$vol_float" 'BEGIN { printf "%d", v*100 }')
+  # Get volume as float (0–1)
+  vol_float=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2}')
+  vol_percent=$(awk -v v="$vol_float" 'BEGIN { printf "%d", v*100 }')
 
-    # Check mute state properly
-    mute_state=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -o 'MUTED')
+  # Check mute state properly
+  mute_state=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -o 'MUTED')
 
-    if [ "$mute_state" = "MUTED" ]; then
-        bg="FF0000"
-        stat="󰟎"
-    else
-        stat="󰋋 $vol_percent%"
-    fi
+  if [ "$mute_state" = "MUTED" ]; then
+    bg="FF0000"
+    stat="󰟎"
+  else
+    stat="󰋋 $vol_percent%"
+  fi
 
-    common
+  common
 }
 
 ipaddr() {
@@ -47,11 +47,11 @@ ipaddr() {
 }
 
 disk() {
-    local name="id_disk"
-    # Get disk usage percentage for root
-    USAGE=$(df / -h | tail -1 | awk '{print $3}')
-    local stat=" $USAGE"
-    common
+  local name="id_disk"
+  # Get disk usage percentage for root
+  USAGE=$(df / -h | tail -1 | awk '{print $4}')
+  local stat=" $USAGE"
+  common
 }
 
 player() {
@@ -60,18 +60,18 @@ player() {
   local stat="$PLAYER"
   common
 }
- 
+
 echo '{ "version": 1 , "click_events":false}'
 echo '[[],'
- 
+
 while :
 do
-echo -n "["
-player
-volume
-ipaddr
-disk
-mydate
-echo -n "],"
-sleep 1
+  echo -n "["
+  player
+  volume
+  ipaddr
+  disk
+  mydate
+  echo -n "],"
+  sleep 1
 done
